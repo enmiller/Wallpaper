48d2a9826754029596755d9a6732455c7506ed6a
Eric Miller <eric.miller@mutualmobile.com>
2015-08-26 04:38:16 -0500

    Fixing failing tests

--- Wallpaper/Wallpaper.swift
+++ Wallpaper/Wallpaper.swift
@@ -95,7 +95,7 @@ public struct WPHTMLOptions : OptionSetType, BooleanType {
     public init(rawValue: UInt) {
         self.rawValue = rawValue
     }
-
+    
     private enum Feature: String {
         case Links = "link"
         case Emphasis = "decorate"
@@ -108,10 +108,10 @@ public struct WPHTMLOptions : OptionSetType, BooleanType {
         case AllCaps = "allcaps"
         case PrudeText = "prude"
     }
-
+    
     public func toURLPathString() -> String {
         var optionsString: NSString = ""
-
+        
         if self.contains(WPHTMLOptions.AnchorTags) {
             optionsString = optionsString.stringByAppendingPathComponent(Feature.Links.rawValue)
         }
@@ -142,16 +142,16 @@ public struct WPHTMLOptions : OptionSetType, BooleanType {
         if self.contains(WPHTMLOptions.Prude) {
             optionsString = optionsString.stringByAppendingPathComponent(Feature.PrudeText.rawValue)
         }
-
+        
         return optionsString as String
     }
 }
 
 public class Wallpaper: NSObject {
-
+    
     private class func requestImage(path: String, size: CGSize, completion: (UIImage?) -> ()) {
         let screenScale: CGFloat = UIScreen.mainScreen().scale
-
+        
         let urlString = NSString(format: path, "\(Int(size.width * screenScale))", "\(Int(size.height * screenScale))")
         let url = NSURL(string: urlString as String)
         let request = NSURLRequest(URL: url!)
@@ -173,41 +173,41 @@ public class Wallpaper: NSObject {
 
 //MARK: - Images
 public extension Wallpaper {
-
+    
     public class func placeKittenImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.PlaceKitten as String, size: size, completion: completion)
     }
-
+    
     public class func placeKittenGreyscaleImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.PlaceKittenGreyscale as String, size: size, completion: completion)
     }
-
+    
     public class func placeBaconImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.Bacon as String, size: size, completion: completion)
     }
-
+    
     public class func placeHolderImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.PlaceHolder as String, size: size, completion: completion)
     }
-
+    
     public class func placeRandomImage(size: CGSize, category: String, completion: (image: UIImage?) -> ()) {
         let path = WallpaperImageURLString.Random.stringByAppendingPathComponent(category)
         requestImage(path, size: size, completion: completion)
     }
-
+    
     public class func placeRandomGreyscaleImage(size: CGSize, category: String, completion: (image: UIImage?) -> ()) {
         let path = WallpaperImageURLString.RandomGreyscale.stringByAppendingPathComponent(category)
         requestImage(path, size: size, completion: completion)
     }
-
+    
     public class func placeRandomImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.Random as String, size: size, completion: completion)
     }
-
+    
     public class func placeRandomGreyscaleImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.RandomGreyscale as String, size: size, completion: completion)
     }
-
+    
     public class func placeDowneyImage(size: CGSize, completion: (image: UIImage?) -> ()) {
         requestImage(WallpaperImageURLString.Downey as String, size: size, completion: completion)
     }
@@ -216,14 +216,14 @@ public extension Wallpaper {
 
 //MARK: - Text
 public extension Wallpaper {
-
+    
     public class func placeText(numberOfParagraphs: Int, paragraphLength: WPTextParagraphLength, textOptions: WPTextOptions, completion: (String?) -> ()) {
         assert(numberOfParagraphs > 0, "Number of paragraphs is invalid")
-
+        
         var urlString: NSString = kWPPlaceRandomTextURLString.stringByAppendingString(textOptions.toMaskString())
         urlString = urlString.stringByAppendingPathComponent("plaintext")
         urlString = urlString.stringByAppendingPathComponent(paragraphLength.rawValue)
-
+        
         let paragraphArgs = "\(numberOfParagraphs)"
         urlString = urlString.stringByAppendingPathComponent(paragraphArgs)
         let url = NSURL(string: urlString as String)
@@ -242,7 +242,7 @@ public extension Wallpaper {
             }
         }
     }
-
+    
     public class func placeHipsterIpsum(numberOfParagraphs: Int, shotOfLatin: Bool, completion: (String?) -> ()) {
         var hipsterPath: String = "http://hipsterjesus.com/api?paras=\(numberOfParagraphs)&html=false"
         if shotOfLatin {
@@ -256,19 +256,14 @@ public extension Wallpaper {
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
             if error == nil {
                 if let unwrappedData = data {
-                    
-                    do {
-                        if let dict = try NSJSONSerialization.JSONObjectWithData(unwrappedData, options: NSJSONReadingOptions(rawValue: 0)) as? [String : String] {
-                            let returnString = dict["text"]
-                            completion(returnString)
-                        } else {
-                            completion(nil)
-                        }
-                    } catch {
-                        
+                    if let JSONDict = try? NSJSONSerialization.JSONObjectWithData(unwrappedData, options: NSJSONReadingOptions(rawValue: 0)),
+                        let dict = JSONDict as? [String : AnyObject]
+                    {
+                        let returnString = dict["text"] as? String
+                        completion(returnString)
+                    } else {
+                        completion(nil)
                     }
-                } else {
-                    completion(nil)
                 }
             } else {
                 print("\(__FUNCTION__) Wallpaper Error: \(error)")
@@ -276,7 +271,7 @@ public extension Wallpaper {
             }
         }
     }
-
+    
     public class func placeHTML(numberOfParagraphs: Int, paragraphLength: WPTextParagraphLength, options: WPHTMLOptions, completion: (String?) -> ()) -> () {
         let htmlURL = placeURLForHTML(paragraphLength, htmlOptions: options)
         let request = NSURLRequest(URL: htmlURL)
@@ -294,66 +289,66 @@ public extension Wallpaper {
             }
         }
     }
-
+    
     public class func placeURLForHTML(paragraphLength: WPTextParagraphLength, htmlOptions: WPHTMLOptions) -> NSURL {
         let htmlURLString = kWPPlaceRandomTextURLString
         let optionsString = htmlOptions.toURLPathString()
-
+        
         let lengthURLString = htmlURLString + "\(paragraphLength.rawValue)"
         let fullURLString = lengthURLString + "/\(optionsString)"
-
+        
         return NSURL(string: fullURLString)!
     }
 }
 
 //MARK: - Colors
 public extension Wallpaper {
-
+    
     public class func placeRandomColorWithHue(hue: CGFloat) -> UIColor {
         assert(hue <= 1 && hue >= 0, "Hue value must be between 0 and 1")
-
+        
         let upperLimit = 100
         let lowerLimit = 10
-
+        
         let percentRange = NSMakeRange(lowerLimit, upperLimit - lowerLimit)
-
+        
         let s = randomPercentage(percentRange)
         let b = randomPercentage(percentRange)
-
+        
         return UIColor(hue: hue, saturation: s, brightness: b, alpha: 1.0)
     }
-
+    
     public class func placeRandomColor() -> UIColor {
         return placeRandomColorWithAlpha(1.0)
     }
-
+    
     public class func placeRandomColorWithAlpha(alpha: CGFloat) -> UIColor {
         let r = randomPercentage()
         let g = randomPercentage()
         let b = randomPercentage()
-
+        
         return UIColor(red: r, green: g, blue: b, alpha: alpha)
     }
-
+    
     public class func placeRandomColorWithRandomAlpha() -> UIColor {
         let alpha = max(randomPercentage(), CGFloat(ColorAlphaLimit))
         return placeRandomColorWithAlpha(alpha)
     }
-
+    
     public class func placeRandomGreyscaleColor() -> UIColor {
         return placeRandomGreyscaleColor(1.0)
     }
-
+    
     public class func placeRandomGreyscaleColor(alpha: CGFloat) -> UIColor {
         let greyness = min(max(randomPercentage(), CGFloat(ColorAlphaLimit)), CGFloat((1.0 - ColorAlphaLimit)))
         return UIColor(white: greyness, alpha: alpha)
     }
-
+    
     public class func placeRandomGreyscaleColorWithRandomAlpha() -> UIColor {
         let alpha = max(randomPercentage(), CGFloat(ColorAlphaLimit))
         return placeRandomGreyscaleColor(alpha)
     }
-
+    
     public class func placeRandomColorWithHueOfColor(color: UIColor) -> UIColor {
         var hue: CGFloat = 0.0
         color.getHue(&hue, saturation: nil, brightness: nil, alpha: nil)
@@ -363,28 +358,28 @@ public extension Wallpaper {
 
 //MARK: - Private Random Number Helpers
 private extension Wallpaper {
-
+    
     private class func randomPhoneNumber() -> String {
         return "(\(arc4random_uniform(000))) \(arc4random_uniform(999))-\(arc4random_uniform(9999))"
     }
-
+    
     private class func randomInteger(lessThan: UInt32) -> Int {
         return Int(arc4random_uniform(lessThan))
     }
-
+    
     private class func randomFloat(lessThan: UInt32) -> CGFloat {
         return (CGFloat(arc4random_uniform(lessThan)) + randomPercentage())
     }
-
+    
     private class func randomFloat(range: NSRange) -> CGFloat {
         
         return ((CGFloat(range.location) + CGFloat(arc4random_uniform(UInt32(range.length)))) + randomPercentage())
     }
-
+    
     private class func randomPercentage() -> CGFloat {
         return (CGFloat(arc4random_uniform(100)) / 100.0)
     }
-
+    
     private class func randomPercentage(range: NSRange) -> CGFloat {
         return ((CGFloat(range.location) + CGFloat(arc4random_uniform(UInt32(range.length)))) / 100.0)
     }
